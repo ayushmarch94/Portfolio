e7e8c25adc27903573a33d336c2d15d686a0a387
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-09-20 21:09:58 +0530

    contact form added

--- src/components/Contact-me/Contact.jsx
+++ src/components/Contact-me/Contact.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import "./Contact.css";
+import { send } from "@emailjs/browser";
 
 export default function Contact() {
-  const redirect = () => {
-    window.open("https://www.linkedin.com/in/ayush-kumar-484013264/", "_blank");
-  };
 
   return (
     <>
@@ -20,13 +18,12 @@ export default function Contact() {
         className="face-main"
       >
         <div className="contact">
-          <form className="ContactBox">
+          <form action="https://formspree.io/f/xbjvzkja" method="POST" className="ContactBox">
             <h1>Contact Me</h1>
-            <input placeholder="NAME" type="text" id="name" />
-            <input placeholder="SUBJECT" type="text" id="Subject" />
-            <input placeholder="YOUR EMAIL ID" type="text" id="emailAddress" />
-            <textarea placeholder="ENTER MESSAGE" type="text" id="message" />
-            <button>Submit</button>
+            <input name="name" placeholder="NAME" type="text" id="name" />
+            <input name="email" placeholder="YOUR EMAIL ID" type="email" id="emailAddress" />
+            <textarea name="message" placeholder="ENTER MESSAGE" type="text" id="message" />
+            <button  type="submit" value={send}>Submit</button>
           </form>
         </div>
       </div>

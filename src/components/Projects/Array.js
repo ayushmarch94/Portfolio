d507a0619be4ca96ae160b98a87fee8c3fdcbdfa
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-10-07 19:55:52 +0530

    updated projects

--- src/components/Projects/Array.js
+++ src/components/Projects/Array.js
@@ -1,33 +1,36 @@
 const Array = [
     {
       key: 1,
-      image: require("../../images/portfolio.png"),
-      code: "https://github.com/ayushmarch94/Portfolio",
-      site: "https://ayush94.netlify.app/",
-    },
-    {
-      key: 2,
-      image: require("../../images/calculator.png"),
-      code: "https://github.com/ayushmarch94/Calculator",
-      site: "https://calculator-ayush-k.netlify.app/",
-    },
-    {
-      key: 3,
       image: require("../../images/weather.png"),
       code: "https://github.com/ayushmarch94/Weather-app",
       site: "https://weather-app-ayushk.netlify.app/",
     },
+
     {
-      key: 4,
-      image: require("../../images/todo.png"),
-      code: "https://github.com/ayushmarch94/To-do-list",
-      site: "https://to-do-list-by-ayush.netlify.app/",
-    },
-    {
-      key: 5,
+      key: 2,
       image: require("../../images/password.png"),
       code: "https://github.com/ayushmarch94/Password-Generator",
       site: "https://password-generator-app-ayush.netlify.app/",
+    },
+    {
+      key: 3,
+      image: require("../../images/calculator.png"),
+      code: "https://github.com/ayushmarch94/Calculator",
+      site: "https://calculator-ayush-k.netlify.app/",
+    },
+
+    {
+      key: 4,
+      image: require("../../images/digitalclock.png"),
+      code: "https://github.com/ayushmarch94/DigitalClock",
+      site: "https://digitalclock-ayush.netlify.app/",
+    },
+
+    {
+      key: 5,
+      image: require("../../images/todo.png"),
+      code: "https://github.com/ayushmarch94/To-do-list",
+      site: "https://to-do-list-by-ayush.netlify.app/",
     },
   ];
   
